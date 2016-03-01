16322918659f3acdb959a0997c7491992641be8b
Kevin Sivabalan <kevin.sivabalan@rangle.io>
2016-03-01 14:39:57 -0500

    Moving app

--- app/components/app.component.ts
+++ app/components/app.component.ts
@@ -1,15 +1,15 @@
 import { RouteConfig, ROUTER_DIRECTIVES, ROUTER_PROVIDERS } from 'angular2/router';
 
 import { Component }       from 'angular2/core';
-import { HeroService } from '../../core/services/hero.service';;
-import { HeroesComponent } from '../heroes/heroes.component';
-import { DashboardComponent } from '../dashboard/dashboard.component';
-import { HeroDetailComponent } from '../hero-detail/hero-detail.component';
+import { HeroService } from '../core/services/hero.service';;
+import { HeroesComponent } from './heroes/heroes.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { HeroDetailComponent } from './hero-detail/hero-detail.component';
 
 @Component({
   selector: 'my-app',
-  styleUrls: ['app/components/app/app.component.css'],
-  templateUrl: 'app/components/app/app.component.html',
+  styleUrls: ['app/components/app.component.css'],
+  templateUrl: 'app/components/app.component.html',
   directives: [
     ROUTER_DIRECTIVES
   ],
